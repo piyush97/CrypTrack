b15ca5b4214dfd7c1dc4712a16871f28d88721d4
Piyush Mehta <me@piyushmehta.com>
2018-05-05 18:13:42 +0530

    NPM Spinner added

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,14 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+import FetchCoinData from './../Actions/FetchCoinData';
+import CoinCard from './CoinCard';
 
 class CryptoContainer extends Component {
+
+    componentWillMount() {
+        this.props.FetchCoinData();
+    }
+
+    renderCoinCards() {
+        const { crypto } = this.props;
+        return crypto.data.map((coin) => 
+            <CoinCard 
+                key={coin.name}
+                coin_name={coin.name}
+                symbol={coin.symbol}
+                price_usd={coin.price_usd}
+                percent_change_24h={coin.percent_change_24h}
+                percent_change_7d={coin.percent_change_7d}
+            />
+        ) 
+    }
+
+
     render() {
+
+        const { crypto } = this.props;
+        const { contentContainer } = styles;
+
+        if (crypto.isFetching) {
+            return (
+                <View>
+                    <Spinner
+                        visible={crypto.isFetching}
+                        textContent={"Loading..."}
+                        textStyle={{color: '#253145'}}
+                        animation="fade"
+                    />
+                </View>
+            )
+        }
+
         return (
-            <View>
-                <Text>Container</Text>
-            </View>
+            <ScrollView contentContainerStyle={contentContainer}>
+                {this.renderCoinCards()}
+            </ScrollView>
         )
+        
+
+    }
+}
+
+const styles = {
+    contentContainer: {
+        paddingBottom: 100,
+        paddingTop: 55
     }
 }
 
@@ -18,4 +68,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(CryptoContainer)
+export default connect(mapStateToProps, { FetchCoinData })(CryptoContainer)
